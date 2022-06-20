bbf03cad73c5af405948ea286197741dc59196f7
TakauKarin <107631433+TakauKarin@users.noreply.github.com>
2022-06-21 03:34:10 +0900

    Update m2-2.php

--- mission_2/m2-2.php
+++ mission_2/m2-2.php
@@ -3,12 +3,12 @@
 <head>
     <meta charset="UTF-8">
     <title>mission_2-2</title>
+</head>
+<body>
     <form action="" method="post">
         <input type="text" name="comment" value="コメント">
         <input type="submit" name="submit">
     </form>
-</head>
-<body>
     <?php
         if(empty($_POST["comment"]) == False){
             $comment = $_POST["comment"];
