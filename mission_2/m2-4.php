b5047422d1132791f08a6d9e47979798812c9d66
TakauKarin <107631433+TakauKarin@users.noreply.github.com>
2022-06-21 03:35:27 +0900

    Update m2-4.php

--- mission_2/m2-4.php
+++ mission_2/m2-4.php
@@ -3,12 +3,12 @@
 <head>
     <meta charset="UTF-8">
     <title>mission_2-4</title>
+</head>
+<body>
     <form action="" method="post">
         <input type="text" name="comment" value="コメント">
         <input type="submit" name="submit">
     </form>
-</head>
-<body>
     <?php
         echo "↓みなさんが書き込んでくれたコメントが表示されます<br><br>";
         
